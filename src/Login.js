c5de694d06903d5c85b69571792fe014cb2d72cb
MaanilVerma <MaanilVerma@users.noreply.github.com>
2021-05-15 16:32:07 +0530

    error update

--- src/Login.js
+++ src/Login.js
@@ -2,17 +2,27 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import { auth, provider } from './firebase';
 import './Login.css';
-
-
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Login() {
-  
+  const [state, dispatch] = useStateValue();
 
   const signIn = () => {
     auth
-      .signInWithPopup(provider);
-      
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log('result --> ', result);
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
+
 
   return (
     <div className='login'>
